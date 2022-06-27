f636bdd7041b755d6dcf01b75c4ef6efef738837
sun <962372938@qq.com>
2022-06-27 17:10:03 +0800

    显示登录信息

---
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.controller.interceptor.LoginTicketInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Autowired
+    private LoginTicketInterceptor loginTicketInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+    }
+
+}
--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -150,4 +150,7 @@ public class UserService implements CommunityConstant {
         loginTicketMapper.updateStatus(ticket,1);
 
     }
+    public LoginTicket findLoginTicket(String ticket) {
+        return loginTicketMapper.selectByTicket(ticket);
+    }
 }
